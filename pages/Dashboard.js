2af63aec8b106e8c5375f7b7330625b2e7b15dde
stefan-kanazir <kanazir.stefan@gmail.com>
2022-03-06 19:49:27 +0100

    🚀 Header Component

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Header from '../components/Header'
+import styled from 'styled-components'
 
 const Dashboard = ({address}) => {
   return (
-    <div>{address}</div>
+    <Wrapper>
+      <MainContainer>
+        <Header />
+      </MainContainer>
+    </Wrapper>
   )
 }
 
 export default Dashboard
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: #fff;
+`;
+
+const MainContainer = styled.div`
+  flex: 1;
+`;
