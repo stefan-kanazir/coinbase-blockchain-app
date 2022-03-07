093eae2542399ff9339d105a23952cd110247f45
stefan-kanazir <kanazir.stefan@gmail.com>
2022-03-07 22:53:53 +0100

    Coin Component

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { coins } from '../static/coins'
+import Coin from './Coin'
 
 const Portfolio = () => {
   return (
@@ -26,8 +27,7 @@ const Portfolio = () => {
                 <Divider />
                 <div>{coins.map(coin => (
                     <div>
-                        {/* <Coin coin={coin} /> */}
-                        <h2>{coin.name}</h2>
+                        <Coin coin={coin} />
                         <Divider />
                     </div>   
                 ))}</div>
