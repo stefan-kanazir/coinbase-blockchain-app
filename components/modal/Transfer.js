1746fb527583a25a83399c9d72813e0a984112eb
stefan-kanazir <kanazir.stefan@gmail.com>
2022-03-13 22:38:16 +0100

    Transfer Modal - 'Send' window styles

--- components/modal/Transfer.js
+++ components/modal/Transfer.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { FaWallet } from 'react-icons/fa'
 
 const Transfer = () => {
     const [amount, setAmount] = useState()
+    const [recepient, setRecepient] = useState()
 
   return (
       <Wrapper>
@@ -20,6 +22,39 @@ const Transfer = () => {
                   Amount is required
               </Warning>
           </Ammount>
+          <TransferForm>
+              <Row>
+                  <FieldName>To</FieldName>
+                  <Icon>
+                      <FaWallet />
+                  </Icon>
+                  <Recipient 
+                    placeholder='Address'
+                    value={recepient}
+                    onChange={(e) => setRecepient(e.target.value)}
+                  />
+              </Row>
+              <Divider />
+              <Row>
+                  <FieldName>Pay with</FieldName>
+                  <CoinSelectList>
+                    <Icon>
+                        <img 
+                            src="https://avatars.githubusercontent.com/u/59578556?v=4"
+                            alt=""
+                        />
+                    </Icon>
+                    <CoinName>Ethereum</CoinName>
+                  </CoinSelectList>
+              </Row>
+            </TransferForm>
+            <Row>
+                <Continue>Continue</Continue>
+            </Row>
+            <Row>
+                <BalanceTitle>ETH Balance</BalanceTitle>
+                <Balance>1.24 ETH</Balance>
+            </Row>          
       </Wrapper>
   )
 }
@@ -59,7 +94,7 @@ const FlexInput = styled.input`
     font-size: 1.2rem;
     flex-wrap: wrap;
     text-align: right;
-    min-width: 45%;
+    max-width: 45%;
     margin-right: 1rem;
     font-size: 4.5rem;
     color: #3773f5;
@@ -75,3 +110,93 @@ const Warning = styled.div`
     text-align: center;
     color: #8a919e;
 `;
+
+const Divider = styled.div`
+    border-bottom: 1px solid #282b2f;
+`;
+
+const TransferForm = styled.div`
+    border: 1px solid #282b2f;
+    border-radius: .4rem;
+`;
+
+const Row = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #81919e;
+    padding: 1rem 0;
+    font-size: 1.2rem;
+`;
+
+const FieldName = styled.div`
+    flex: 0.5;
+    padding-left: 2rem;
+`;
+
+const Icon = styled.div`
+    margin-right: 1rem;
+    height: 1.8rem;
+    width: 1.8rem;
+    border-radius: 50%;
+    overflow: hidden;
+    display: grid;
+    place-items: center;
+
+    & > img {
+        height: 120%;
+        width: 120%;
+        object-fit: cover;
+    }
+`;
+
+const Recipient = styled.input`
+    flex: 1;
+    border: none;
+    background: none;
+    outline: none;
+    color: #fff;
+    font-size: 1.2rem;
+    flex-wrap: wrap;
+    margin-right: .5rem;
+`;
+
+const CoinSelectList = styled.div`
+    display: flex;
+    flex: 1.3;
+    height: 100%;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const CoinName = styled.div`
+    flex: 1;
+    border: none;
+    background: none;
+    outline: none;
+    color: #fff;
+    font-size: 1.2rem;
+    flex-wrap: wrap;
+    margin-right: .5rem;
+`;
+
+const Continue = styled.div`
+    color: #fff;
+    width: 100%;
+    background-color: #3773f5;
+    padding: 1rem;
+    text-align: center;
+    border-radius: .4rem;
+    font-size: 1.2rem;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #4a80f6;
+    }
+`;
+
+const BalanceTitle = styled.div``;
+
+const Balance = styled.div``;
